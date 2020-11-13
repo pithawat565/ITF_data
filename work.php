d20180b362c4ab44495f41fd81a25c30e61dc4d7
pithawat565 <71390474+pithawat565@users.noreply.github.com>
2020-11-13 10:38:29 +0700

    Create work.php

---
+++ work.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>ITF Lab</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'pithawat.mysql.database.azure.com', 'pithawat@pithawat', 'a0922734418', 'guestbook', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">name</div></th>
+    <th width="350"> <div align="center">back </div></th>
+    <th width="150"> <div align="center">link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['name'];?></div></td>
+    <td><?php echo $Result['back'];?></td>
+    <td><?php echo $Result['link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
